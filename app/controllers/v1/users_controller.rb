0194454cac35b7de9baa7c1d1b766cbbf90c3385
Herbert Joseph <techniquea2z@gmail.com>
2020-09-25 20:18:20 -0400

    Fix password encryption with sorcery

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,7 +3,6 @@ class V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    binding.pry
     if @user.save!
       render json:  @user.to_json(include: :seller)
     else
@@ -15,7 +14,7 @@ class V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :username, :crypted_password, :email, :phone_number,
+      :username, :password, :email, :phone_number,
       seller_attributes: [
         :studio_size, :business_name, :default_currency, accepted_crypto: []
       ]
