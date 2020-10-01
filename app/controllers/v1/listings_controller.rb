61693d6d516cac7515efc3375464b1b95938cb28
Herbert Joseph <techniquea2z@gmail.com>
2020-10-01 12:40:50 -0400

    Unique validations on every DB unique column

--- app/controllers/v1/listings_controller.rb
+++ app/controllers/v1/listings_controller.rb
@@ -3,10 +3,20 @@ class V1::ListingsController < ApplicationController
     page = params[:page]
     page ||= 1
 
-    @listings = Listing.page(page).per(30)
+    @listings = Listing.includes([:listing_videos, :listing_images]).page(page).per(30)
 
     @listings = ListingSerializer.new(@listings)
 
     render_success(status: :ok, data: @listings)
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+
+    if @listing
+      render_success(status: ok, data: ListingSerializer.new(@listing))
+    else
+      render_error(message: 'listing not found', status: :not_found)
+    end
   end
 end
