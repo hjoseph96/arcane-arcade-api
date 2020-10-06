7a3d882af9ce047626fda0a2c25a1518f0ec4db2
Herbert Joseph <techniquea2z@gmail.com>
2020-10-06 08:52:40 -0400

    Fix review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :author, model: 'User', foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :listing
 end
