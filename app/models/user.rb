17d940fa87c83456a67ab0709b547327afc3df7f
Sasa Sekaric <sasasekaric@hotmail.com>
2020-11-30 20:57:06 +0100

    Fix render_errors to support frontend, Add more validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :seller
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :phone_number, presence: true
